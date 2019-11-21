08473146ff2237b278c2ed035754cd60157dedba
ColbySaxton <42782179+ColbySaxton@users.noreply.github.com>
2019-11-21 18:04:55 -0500

    Creation of heap class.

--- public/src/recommendation.service.js
+++ public/src/recommendation.service.js
@@ -8,10 +8,14 @@ RecommendationService.$inject = ['$http', 'API_PATH'];
 function RecommendationService ($http, API_PATH) {
   var service = this;
 
-  service.getRecommendations = function (userMovieName, UserMovieGenre) {
-    var sameGenreMovies = $http.get(API_PATH + '/movies/genre/' + userMovieGenre).then(function (response) {
+  service.getRecommendations = function (userMovie) {
+    var sameGenreMovies = $http.get(API_PATH + '/movies/genre/' + userMovie.genre).then(function (response) {
       return response.data;
     });
+    var movieList = sameGenreMovies.toArray();
+    for(var movie : movieList) {
+
+    }
     console.log(sameGenreMovies);
     return sameGenreMovies;
   };
@@ -25,3 +29,41 @@ function RecommendationService ($http, API_PATH) {
 }
 
 })();
+
+class Heap {
+
+  var heapSize;
+  var lowestValue;
+  var heap = [];
+
+  constructor(thisHeapSize) {
+    heapSize = thisHeapSize;
+  }
+
+  insertCheck(newValue) {
+    if(heap.length < heapSize) {
+      heap.push(newValue);
+      if(newValue < lowestValue) {
+        lowestValue = newValue;
+      }
+      heap.sort(function(a, b){return b - a});
+    }
+    else if(newValue > lowestValue) {
+      heap.pop();
+      heap.push(newValue);
+      heap.sort(function(a, b){return b - a});
+    }
+  }
+
+  getHeap() {
+    return heap;
+  }
+
+  getLowestValue() {
+    return lowestValue;
+  }
+
+  getHeapSize() {
+    return heapSize;
+  }
+}
