8a59d0082c6cd260b65afbfc11abfbb043fd6544
ColbySaxton <42782179+ColbySaxton@users.noreply.github.com>
2019-11-21 22:03:12 -0500

    Created test button for recommendations.

--- public/src/home.controller.js
+++ public/src/home.controller.js
@@ -13,13 +13,23 @@ function HomeController (UserService, RecommendationService) {
   };
 
 
-//  homeCtrl.getRecommendations = function () {
-//    RecommendationService.getRecommendations().then(function (response) {
-//      homeCtrl.popularNow = response;
-//      console.log(response);
-//      return response;
-//    });
-//  };
+var sampleMovie = {
+  _id: 5da6788b99d3a4650bbfa935
+  Title: "Diary of a Wimpy Kid: The Long Haul"
+  Year: 2017
+  Poster: "https://m.media-amazon.com/images/M/MV5BYmMyZDRlNDktMDVmMS00Mjc2LThkNT..."
+  Plot: "A Heffley family road trip to attend Meemaw's 90th birthday party goes..."
+  Genre: ["Comedy", "Family"]
+  Cast: ["Marsai Martin", "Idara Victor", "Frances Fisher", "Frankie Faison"]
+}
+
+  homeCtrl.getRecommendations = function () {
+    RecommendationService.getRecommendations(sampleMovie).then(function (response) {
+      homeCtrl.getRecommendations = response;
+      console.log(response);
+      return response;
+    });
+  };
 
   homeCtrl.getPopularNow = function () {
     RecommendationService.getPopularNow("2017").then(function (response) {
