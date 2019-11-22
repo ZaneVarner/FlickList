aa2380222c8c4cbf4dfabe31d89212a9f3e0551d
Zane Varner <zanevarner0@gmail.com>
2019-11-22 08:30:45 -0500

    Changed IP Address of server connection to remote.

--- public/src/flicklist.module.js
+++ public/src/flicklist.module.js
@@ -2,7 +2,7 @@
 'use strict';
 
 angular.module('FlickList', ['ui.router', 'firebase', 'MovieSearch'])
-.constant('API_PATH', 'https://localhost:8080');
-// .constant('API_PATH', 'http://18.220.159.104:8080');
+// .constant('API_PATH', 'https://localhost:8080');
+.constant('API_PATH', 'http://18.220.159.104:8080');
 
 })();
