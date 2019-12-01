8f4a8de75851d0738f7a62a737a117468bdd3913
John Shi <johnshi0001@gmail.com>
2019-12-01 01:32:47 -0500

    Brought over HomeController unit test from test branch. Still has bugs to fix.

---
+++ spec/home.controller.spec.js
@@ -0,0 +1,59 @@
+describe('wat', function(){
+  var mockUserService, mockRecService, rootScope, scope, passPromise, secondController;
+
+  beforeEach(function(){
+    module(function($provide){
+      $provide.factory('UserService', ['$q', function($q){
+        function getUser(){
+          if(passPromise){
+            return $q.when();
+          } else {
+            return $q.reject();
+          }
+        }
+
+        return {
+          getUser: getUser
+        };
+      }]);
+
+      $provide.factory('RecommendationService', ['$q', function($q){
+        function getPopularMovies(data){
+          if(passPromise){
+            return $q.when();
+          } else {
+            return $q.reject();
+          }
+        }
+
+        return {
+          getPopularMovies: getPopularMovies
+        };
+      }]);
+    });
+
+    module('FlickList');
+  });
+
+  beforeEach(inject(function($rootScope, $controller, UserService, RecommendationService){
+    rootScope = $rootScope;
+    scope = $rootScope.$new();
+    mockUserService = UserService;
+    mockRecService = RecommendationService;
+  }));
+
+  describe('secondController', function(){
+    beforeEach(inject(function($controller){
+      secondController = $controller('HomeController',{
+        UserService: mockUserService,
+        RecommendationService: mockRecService
+      });
+    }));
+
+    it('should get recommended movies', function(){
+      passPromise = true;
+      secondController.$onInit();
+      rootScope.$digest();
+    });
+  });
+});
